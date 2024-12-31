c5baff03f78702b41ac7130f4202d4d1809d458c
fultonoff <fultonkilebe7@gmail.com>
2024-12-31 20:42:49 +0100

    ✅added contact

--- src/app/(routes)/contact/page.jsx
+++ src/app/(routes)/contact/page.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact
+    <main className='container center'>
+        <h1 className='text-xl md:text-4xl lg:text-6xl font-bold text-mainColortext-center'>Contact 📫</h1>
+
+        <div className='mt-10 flex flex-col gap-5'>
+          <div>
+            <h3>Email:</h3>
+            <p>jilianalnath@yahoo.fr</p>
+            
+          </div>
+          <div>
+            <h3>Telephone:</h3>
+            <p>062505589154</p>
+
+          </div>
+        </div>
+
      
-    </div>
+    </main>
   )
 }
 
